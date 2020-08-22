23cdd7b73ccd29b746b0fb2d57b3e7b1a57e4b00
Dustin Bowers <dustin.uncc@gmail.com>
2020-08-22 05:14:36 -0400

    Add argument-based launch. Slight refactor

--- main.go
+++ main.go
@@ -17,49 +17,31 @@ const (
 var keyMap map[int]uint8
 
 func main() {
+	var romPath string
+	romPath = "roms/games/Space Invaders [David Winter].ch8"
+	if len(os.Args) == 2 {
+		romPath = os.Args[1]
+	}
+
 	var emu chip8.Chip8
+
+	log.Print("Initializing emulator... ")
 	emu.Initialize()
-	//err := emu.LoadRom("roms/demos/Maze [David Winter, 199x].ch8")
-	//err := emu.LoadRom("roms/demos/Zero Demo [zeroZshadow, 2007].ch8")
-	//err := emu.LoadRom("roms/games/Brix [Andreas Gustafsson, 1990].ch8")
-	//err := emu.LoadRom("roms/games/Addition Problems [Paul C. Moews].ch8")
-	//err := emu.LoadRom("roms/games/Space Flight.ch8")
-	//err := emu.LoadRom("roms/games/Cave.ch8")
-	err := emu.LoadRom("roms/games/Pong (1 player).ch8")
-	//err := emu.LoadRom("roms/games/Space Invaders [David Winter].ch8")
-	//err := emu.LoadRom("roms/games/Tetris [Fran Dachille, 1991].ch8")
-	//err := emu.LoadRom("roms/games/Worm V4 [RB-Revival Studios, 2007].ch8")
+	log.Println("Done")
+
+	log.Printf("Loading rom at: %v\n", romPath)
+	err := emu.LoadRom(romPath)
 	if err != nil {
 		os.Exit(1)
 		return
 	}
 
-	keyMap = make(map[int]uint8)
-	keyMap[sdl.K_1] = 0x1
-	keyMap[sdl.K_2] = 0x2
-	keyMap[sdl.K_3] = 0x3
-	keyMap[sdl.K_4] = 0xc
-
-	keyMap[sdl.K_q] = 0x4
-	keyMap[sdl.K_w] = 0x5
-	keyMap[sdl.K_e] = 0x6
-	keyMap[sdl.K_r] = 0xd
-
-	keyMap[sdl.K_a] = 0x7
-	keyMap[sdl.K_s] = 0x8
-	keyMap[sdl.K_d] = 0x9
-	keyMap[sdl.K_f] = 0xe
-
-	keyMap[sdl.K_z] = 0xa
-	keyMap[sdl.K_x] = 0x0
-	keyMap[sdl.K_c] = 0xb
-	keyMap[sdl.K_v] = 0xf
+	keyMap = getKeyMap()
 
 	ui.Init(512, 256, screenCols, screenRows)
 	defer ui.Cleanup()
 
 	running := true
-
 	hz := 700
 	delay := time.Duration(1000 / hz)
 	go func() {
@@ -106,4 +88,28 @@ func main() {
 		}
 		time.Sleep(time.Microsecond * 16700)
 	}
+}
+
+func getKeyMap() map[int]uint8 {
+	keyMap = make(map[int]uint8)
+	keyMap[sdl.K_1] = 0x1
+	keyMap[sdl.K_2] = 0x2
+	keyMap[sdl.K_3] = 0x3
+	keyMap[sdl.K_4] = 0xc
+
+	keyMap[sdl.K_q] = 0x4
+	keyMap[sdl.K_w] = 0x5
+	keyMap[sdl.K_e] = 0x6
+	keyMap[sdl.K_r] = 0xd
+
+	keyMap[sdl.K_a] = 0x7
+	keyMap[sdl.K_s] = 0x8
+	keyMap[sdl.K_d] = 0x9
+	keyMap[sdl.K_f] = 0xe
+
+	keyMap[sdl.K_z] = 0xa
+	keyMap[sdl.K_x] = 0x0
+	keyMap[sdl.K_c] = 0xb
+	keyMap[sdl.K_v] = 0xf
+	return keyMap
 }
